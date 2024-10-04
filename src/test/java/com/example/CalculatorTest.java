7e3f904e9cb9aa5b0aa6644f452b592a44c3e4fd
unknown <Moluwale0487@conestogac.on.ca>
2024-10-03 20:47:01 -0400

    My first commit

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import junit.framework.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        // int result = calculator.add(2, 3);
+        // assertEquals(5, result, "2 + 3 should equal 5");
+        // Assert.assertEquals(5,Calculator.add(2,3));
+
+        Assert.assertEquals(5, calculator.add(3, 2));
+        Assert.assertEquals(1, calculator.sub(3, 2));
+        Assert.assertEquals(6, calculator.multiply(3, 2));
+
+        // Assert.assertSame(1, calculator.div(5, 5));
+        // assertThrows(0, calculator.div(5, 0));
+
+        assertThrows(ArithmeticException.class, () -> {
+            // Code that should throw the exception
+            Calculator cal = new Calculator();
+            cal.div(10, 0); // Example division by zero
+        });
+    }
+}
